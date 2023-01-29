8c2481bcf0de87dc563a054ffb81a1da1af01bea
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2023-01-29 09:21:24 +0200

    Fixed deploy error.

--- src/components/LibraryHeader/LibraryHeader.jsx
+++ src/components/LibraryHeader/LibraryHeader.jsx
@@ -1,5 +1,5 @@
-import { useState, Suspense } from "react";
-import { Outlet, useSearchParams } from "react-router-dom";
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 
 import { Navigation } from "components/Navigation/Navigation";
 import { Audio } from 'react-loader-spinner';
